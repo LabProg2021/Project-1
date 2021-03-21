c0eb4ce6729d58c158aaeddf792191df5fdc07c0
Luís Duarte <luis99freitas@gmail.com>
2021-03-21 18:44:58 +0000

    data.h & src.c (added)

---
+++ data.h
@@ -0,0 +1,20 @@
+#ifndef DATA_H_INCLUDED
+#define DATA_H_INCLUDED
+
+typedef struct{
+int day;
+int month;
+int year;
+} Date;
+
+typedef struct{
+    int id;
+    Date creatDate;
+    char* title;
+    int state;
+    int priority;
+    Date deadline;
+} Card;
+
+
+#endif // DATA_H_INCLUDED
